e3ca0178cbb27c62e6b084553d35048b2ae413a1
Mateusz Wilk <matewilk@gmail.com>
2023-02-18 17:35:04 +0000

    (feat): enable CORS in Next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     deviceSizes: [640, 768, 1024, 1280, 1536],
