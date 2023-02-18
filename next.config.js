0da7da960c5abcfd806721fa531355dceace92ff
Mat Wilk <matewilk@gmail.com>
2023-02-18 17:51:12 +0000

    (feat): multi domain Vercel setup (#8)

--- next.config.js
+++ next.config.js
@@ -3,26 +3,47 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        deviceSizes: [640, 768, 1024, 1280, 1536],
-        loader: "custom",
-        path: "/",
-    },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+  images: {
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+    loader: "custom",
+    path: "/",
+  },
 };
 
 module.exports = withPlugins(
+  [
     [
-        [
-            withPWA,
-            {
-                pwa: {
-                    disable: process.env.NODE_ENV === "development",
-                    dest: "public",
-                    runtimeCaching,
-                },
-            },
-        ],
+      withPWA,
+      {
+        pwa: {
+          disable: process.env.NODE_ENV === "development",
+          dest: "public",
+          runtimeCaching,
+        },
+      },
     ],
-    nextConfig
+  ],
+  nextConfig
 );
